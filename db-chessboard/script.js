8e0d784f23afd8764af79defa4633efd8813fe55
Tilmann Singer <tils@tils.net>
2023-05-11 22:27:38 +0200

    Use searchParams instead
    Ha, the page before links to
    https://developer.mozilla.org/en-US/docs/Web/API/URL/searchParams#examples
    that documents a nicer way. One line less, but more important, looks
    more readable to me.

--- db-chessboard/script.js
+++ db-chessboard/script.js
@@ -1,7 +1,6 @@
 document.onreadystatechange = () => {
   if (document.readyState === "interactive") {
-    const queryString = window.location.search;
-    const params = new URLSearchParams(queryString);
+    const params = new URL(document.location).searchParams;
     const start = params.get("start");
 
     console.log("the start param is", start);
